05fadc87152bfb997763b1ba7e97397194833023
tchappel <thomas.chappel4@gmail.com>
2025-11-28 14:19:37 +0100

    add photo-gallery to homepage

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,12 @@
-import { Gallery } from "./components/gallery";
+import { PhotoGallery } from "@/components/photo-gallery";
+import { homeGallery } from "@/lib/home-gallery";
 import { HeroGallery } from "./components/hero-gallery";
 
 export default function Home() {
   return (
-    <div>
+    <>
       <HeroGallery />
-      <Gallery />
-    </div>
+      <PhotoGallery photos={homeGallery} />
+    </>
   );
 }
