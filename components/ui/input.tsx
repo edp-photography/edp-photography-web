a63e21e42445c07237a520fb03365a88fdaabf5d
tchappel <thomas.chappel4@gmail.com>
2025-12-02 13:22:16 +0100

    remove unnecessary border

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -9,22 +9,18 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
       data-slot="input"
       className={cn(
         /* Layout */
-        "h-9 w-full min-w-0 px-3 py-1",
-        "file:inline-flex file:h-7",
+        "w-full min-w-0 h-9 px-3 py-1 file:inline-flex file:h-7",
         /* Positioning */
         /* Typography */
-        "text-base md:text-sm file:text-sm",
-        "file:font-medium",
-        "placeholder:text-muted-foreground file:text-foreground",
+        "font-sans text-sm file:text-sm file:font-medium placeholder:text-muted-foreground file:text-foreground",
         /* Visual styles / Appearance */
         "bg-transparent dark:bg-input/30 file:bg-transparent",
-        "border border-input file:border-0 rounded-md",
-        "shadow-xs outline-none",
+        "border-b border-foreground file:border-0 rounded-0",
         /* Interactivity */
-        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         "selection:bg-primary selection:text-primary-foreground",
         /* Transitions & animations */
-        "transition-[color,box-shadow]",
+        "transition-colors",
         /* State variants */
         "disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         "aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40",
