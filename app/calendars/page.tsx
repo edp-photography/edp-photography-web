bc3885bd19eb0d6441eaabbcb066f5a285c10d45
tchappel <thomas.chappel4@gmail.com>
2026-02-08 15:22:10 +0100

    use StrapiImage in Calendars Page

--- app/calendars/page.tsx
+++ app/calendars/page.tsx
@@ -1,22 +1,12 @@
+import { StrapiImage } from "@/components/strapi-image";
 import { getCalendarsPage } from "@/data/calendars";
-import { toAbsoluteUrl } from "@/lib/strapi/utils";
 import { cn } from "@/lib/utils";
 import { typography } from "@/lib/variants/typography";
-import Image from "next/image";
 
 export default async function CalendarsPage() {
   const calendarsPage = await getCalendarsPage();
 
-  const calendarImages = calendarsPage.data.imageGallery?.images?.map(
-    (image) => ({
-      title: image.title,
-      description: image.description,
-      alt: image.image.alternativeText ?? "",
-      src: toAbsoluteUrl(image.image.url),
-      width: image.image.width!,
-      height: image.image.height!,
-    }),
-  );
+  const calendarImages = calendarsPage.data.imageGallery?.images ?? [];
 
   return (
     <section className="container-fluid mx-auto my-8">
@@ -25,41 +15,47 @@ export default async function CalendarsPage() {
       </h2>
       {calendarImages && calendarImages.length > 0 && (
         <div className="space-y-12 max-w-2xl mx-auto">
-          {calendarImages.map((image, index) => (
-            <figure key={image.src} className="w-full">
+          {calendarImages.map(({ image, title, description }, index) => (
+            <figure key={index} className="w-full">
               <div
                 className="relative w-full"
                 style={{ aspectRatio: `${image.width} / ${image.height}` }}
               >
-                <Image
+                <StrapiImage
+                  image={image}
+                  format="medium"
                   fill
-                  className="object-cover"
-                  src={image.src}
-                  alt={image.alt}
-                  sizes="(max-width: 1536px) 100vw, 1536px"
-                  priority={index < 2}
+                  className="object-cover md:hidden"
+                  preload={index < 2}
+                />
+                <StrapiImage
+                  image={image}
+                  format="large"
+                  fill
+                  className="object-cover max-md:hidden"
+                  preload={index < 2}
                 />
               </div>
 
               <figcaption className="py-6">
-                {image.title && (
+                {title && (
                   <h3
                     className={cn(
                       typography({ variant: "h6", disableGutters: true }),
                       "font-bold",
                     )}
                   >
-                    {image.title}
+                    {title}
                   </h3>
                 )}
-                {image.description && (
+                {description && (
                   <p
                     className={cn(
                       typography({ variant: "caption", disableGutters: true }),
                       "uppercase",
                     )}
                   >
-                    {image.description}
+                    {description}
                   </p>
                 )}
               </figcaption>
